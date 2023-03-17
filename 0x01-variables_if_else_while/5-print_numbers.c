ebc7c5b25cb1163bfd8aebeadc6ff15398005a66
Lionel <ndabagajeanlionel@gmail.com>
2023-03-17 19:32:28 +0000

    print numbers fixed

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print all single numbers in base 10
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
