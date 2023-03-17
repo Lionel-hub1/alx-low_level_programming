05d79aaa7f5afd3d643cbeee92d294a4110417af
Lionel <ndabagajeanlionel@gmail.com>
2023-03-17 18:21:33 +0000

    Print lower and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print alphabets in lower and uppercase
+ * Return: It returns 0
+ */
+int main(void)
+{
+	char alphabets[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i = 0;
+
+	while (alphabets[i] != '\0')
+	{
+		putchar(alphabets[i]);
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
