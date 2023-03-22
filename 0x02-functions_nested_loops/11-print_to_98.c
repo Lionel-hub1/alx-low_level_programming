2ec6540ca720aca76e9ce9eaa6e24c03c19a1d0e
Lionel <ndabagajeanlionel@gmail.com>
2023-03-22 18:38:13 +0000

    Number to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,43 +4,40 @@
  * print_to_98 - This program prints all natural numbers from n to 98
  * @n: print from this number
  */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+	}
+	if (n > 9)
+	{
+		print_number(n / 10);
+	}
+	putchar(n % 10 + '0');
+}
+
 void print_to_98(int n)
 {
-	int i, j;
-
+	int i;
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
-		{
-			if (i != 98)
-			{
-				_putchar(i + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			else if (i == 98)
-			{
-				_putchar(i + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	} else if (n >= 98)
-	{
-		for (j = n; j >= 98; j--)
-		{
-			if (j != 98)
-			{
-				_putchar(j + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			else if (j == 98)
-			{
-				_putchar(j + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
+		for (i = n; i < 98; i++) {
+			print_number(i);
+			putchar(',');
+			putchar(' ');
 		}
 	}
+	else
+	{
+		for (i = n; i > 98; i--)
+		{
+			print_number(i);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	print_number(98);
+	putchar('\n');
 }
