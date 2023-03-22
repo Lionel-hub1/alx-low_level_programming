b5e1df04d3d2918d3c06157b6b0816de840a109f
Lionel <ndabagajeanlionel@gmail.com>
2023-03-22 19:19:17 +0000

    Natural numbers below 10

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - The program list all the natural numbers below 10 and % (3*5))=0
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
