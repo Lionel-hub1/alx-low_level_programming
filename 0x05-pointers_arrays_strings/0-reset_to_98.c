7aaea94d7c048255050c37cfc16571c7456e0079
Lionel <ndabagajeanlionel@gmail.com>
2023-03-27 23:32:01 +0000

    Printing the changed value successfully

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Is a function that modifies a value of argument
+ * @n: this is the integer's location
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
