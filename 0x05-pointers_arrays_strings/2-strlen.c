62777d258b61954ed8f869de8850b5c9252b2852
Lionel <ndabagajeanlionel@gmail.com>
2023-03-28 00:18:41 +0000

    Prints the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen - This function returns the length of string
+ * @s: This is the string to be computed
+ * Return: lenghth of the string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
