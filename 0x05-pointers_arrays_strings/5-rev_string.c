02982e5853c9250040245cc988c72831d6ff448c
Lionel <ndabagajeanlionel@gmail.com>
2023-03-28 00:52:34 +0000

    Task done

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * rev_string - This function reverses a string
+ * @s: Is the string to be reversed
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int length;
+
+	for (length = strlen(s); length >= 0; length--)
+	{
+		_putchar(s[length]);
+	}
+	_putchar('\n');
+}
