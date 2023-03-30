613ea61e1f0f4fc03e9eb45e01efa361d4d69280
Lionel <ndabagajeanlionel@gmail.com>
2023-03-30 23:46:19 +0000

    Task done

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,29 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * rot13 - This is a function that encodes a string using rot13
+ * rot13 - This function encodes a string using rot13
  * @string: The string to be encoded
- * Return: The encoded string
+ * Return: Encoded string
  */
+
 char *rot13(char *string)
 {
-	char replace[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char using[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	int i, j;
+	char replace[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char use[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	i = 0;
 	while (string[i] != '\0')
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (string[i] == replace[i])
+			if (string[i] == replace[j])
 			{
-				string[i] = using[j];
+				string[i] = use[j];
 				break;
 			}
 		}
 		i++;
 	}
-
 	return (string);
 }
