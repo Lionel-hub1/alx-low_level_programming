56570104449f936272351ccc05214608f6affe0d
Lionel <ndabagajeanlionel@gmail.com>
2023-04-03 15:00:16 +0000

    The output comes as expected

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - This function fills the memory with a constant byte
+ * @s: The location to copy to
+ * @b: The constant byte to copy
+ * @n: The number of characters to ocupy
+ * Return: the pointer to the transformed string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
