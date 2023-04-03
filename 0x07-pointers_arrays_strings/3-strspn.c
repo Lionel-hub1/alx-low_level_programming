443083212320c1fcdabe117541b822d431a94f40
Lionel <ndabagajeanlionel@gmail.com>
2023-04-03 17:53:35 +0000

    Task done

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,16 +15,14 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accept[j]) {
+			if (s[i] == accept[j])
+			{
 				count++;
 				break;
 			}
 		}
 		if (accept[j] == '\0')
-		{
 			break;
-		}
 	}
-    
 	return (count);
 }
