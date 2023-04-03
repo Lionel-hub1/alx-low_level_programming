ffd46eee4e29f46d251427104e684a88037f8ef1
Lionel <ndabagajeanlionel@gmail.com>
2023-04-03 19:35:57 +0000

    The expected output printing

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *  _strstr - This function locates a substring
+ *  @haystack: The larger string
+ *  @needle: The substring to search for
+ *  Return: A pointer to where @neddle begins
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+	char *found;
+
+	for (i = 0; *(haystack + i) != '\0'; i++)
+	{
+		j = 0;
+		if (*(haystack + i) == *(needle + j))
+		{
+			found = &haystack[i];
+			for (; *(needle + j) >= '\0'; j++)
+			{
+				if (*(needle + j) == '\0')
+					return (found);
+			}
+		}
+	}
+	return ('\0');
+}
