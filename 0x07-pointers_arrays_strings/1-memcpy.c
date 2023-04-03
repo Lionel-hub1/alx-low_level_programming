9bdbadc3845977f36f0e02ce00b1446ecfbf6e75
Lionel <ndabagajeanlionel@gmail.com>
2023-04-03 15:52:03 +0000

    Task done

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,10 +10,13 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
+	unsigned int j;
+
 
 	for (i = 0; i < n; i++)
 	{
-		*(src + i) = *(dest + i);
+		*(src + i) = *(dest + j);
+		j++;
 	}
 	return (dest);
 }
