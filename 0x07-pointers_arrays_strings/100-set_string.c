cc319e5f78d1564952ec560997997b934fcd1e06
Lionel <ndabagajeanlionel@gmail.com>
2023-04-04 01:21:50 +0000

    Transform successful

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - This function sets a value of a pointer to a char
+ * @s: The string to transform
+ * @to: The string to become
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
