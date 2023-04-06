bf7f3e3da27f5c56464a4301b4f14f0fc1532fec
Lionel <ndabagajeanlionel@gmail.com>
2023-04-06 02:11:01 +0000

    Task done

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,31 +12,31 @@ int wildcmp(char *s1, char *s2)
 	{
 		if (*s2 == '\0')
 		{
-			return 1;
+			return (1);
 		}
 		else if (*s2 == '*')
 		{
-			return wildcmp(s1, s2 + 1);
+			return (wildcmp(s1, s2 + 1));
 		}
 		else
 		{
-			return 0;
+			return (0);
 		}
 	}
 	else if (*s2 == '\0')
 	{
-		return 0;
+		return (0);
 	}
 	else if (*s2 == '*')
 	{
-		return wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1);
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
 	else if (*s1 == *s2)
 	{
-		return wildcmp(s1 + 1, s2 + 1);
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 }
