5f8af78ecd35204818d4afcad172288f8d2ea3f7
Lionel <ndabagajeanlionel@gmail.com>
2023-04-10 23:26:20 +0000

    Library set successfully

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - This program checks for uppercase character.
+ * @c: an integer
+ * Return: This returns 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
