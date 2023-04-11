227f9aab23356f9f71bb7b058f44093630b12b07
Lionel <ndabagajeanlionel@gmail.com>
2023-04-11 23:05:21 +0000

    Returning the right pointer

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strdup - This function shows a pointer to a newly allocated space
+ * @str: The string that is going to be duplicated
+ * Return: The pointer to the memory of duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *dup_ptr;
+	int length, i;
+
+	length = 0;
+	/*As long as the string havent ended(on last '\0') increment length*/
+	while (str[length] != '\0')
+		length++;
+
+	dup_ptr = malloc(sizeof(char) * ++length);
+	if (dup_ptr == NULL || str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		dup_ptr[i] = str[i];
+
+	return (dup_ptr);
+}
