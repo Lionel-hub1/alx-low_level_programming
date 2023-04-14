6843f6576ecb773e2e1d23ed404bad7b9901108b
Lionel <ndabagajeanlionel@gmail.com>
2023-04-14 15:46:25 +0000

    Not including null character in length

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,9 +14,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	length1 = 0;
 	length2 = 0;
-	while (s1[length1])
+	while (s1[length1] != '\0')
 		length1++;
-	while (s2 && s2[length2])
+	while (s2[length2] != '\0')
 		length2++;
 
 	if (n < length2)
