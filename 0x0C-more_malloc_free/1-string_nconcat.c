3208020ee4643d60adb135bb21a6aecb874a7922
Lionel <ndabagajeanlionel@gmail.com>
2023-04-16 13:44:22 +0000

    NULL qualifies an empty string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,6 +12,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *whole;
 	unsigned int i, j, length1, length2;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	for (length1 = 0; s1[length1] != '\0'; length1++)
 		;
 	for (length2 = 0; s2[length2] != '\0'; length2++)
