d5e6be9aa7452bdf0e4512ede395af4eec9c3ab6
Lionel <ndabagajeanlionel@gmail.com>
2023-04-16 13:40:22 +0000

    Corcted betty

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
-		return NULL;
+		return (NULL);
 
 	a_array = malloc(size * nmemb);
 	if (a_array == NULL)
