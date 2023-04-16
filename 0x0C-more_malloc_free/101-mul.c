d5e786886215d33fd5319aec1aac68079bf1cb29
Lionel <ndabagajeanlionel@gmail.com>
2023-04-16 18:53:13 +0000

    More improvements

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
-/*
+/**
  * is_valid_input - This function checks if the argument contains only digits
  * @s: the input string to check
  * Return: true if the string contains only digits, false otherwise
  */
-bool is_valid_input(char* s)
+bool is_valid_input(char *s)
 {
 	int i = 0;
 
@@ -18,15 +18,14 @@ bool is_valid_input(char* s)
 	return (true);
 }
 
-/*
+/**
  * multiply - This function multiplies two numbers in form of strings
  * @s1: The first input string
  * @s2: The second input string
  * Return: void
  */
-void multiply(char* s1, char* s2)
+void multiply(char *s1, char *s2)
 {
-	/* Calculate the lengths of the input strings */
 	int i, j, len1 = 0, len2 = 0, full_len, *result;
 	bool leading_zeros;
 
@@ -34,19 +33,19 @@ void multiply(char* s1, char* s2)
 		len1++;
 	while (s2[len2] != '\0')
 		len2++;
-	
-	/* Allocate an array to store the result */
+
 	full_len = len1 + len2 + 1;
 	result = calloc(full_len, sizeof(int));
 
 	/* Perform the multiplication */
 	for (i = len1 - 1; i >= 0; i--)
 	{
-		int digit1 = s1[i] - '0';
-		int carry = 0;
+		int carry = 0, digit1 = s1[i] - '0';
+
 		for (j = len2 - 1; j >= 0; j--)
 		{
 			int digit2 = s2[j] - '0';
+
 			carry += result[i + j + 1] + (digit1 * digit2);
 			result[i + j + 1] = carry % 10;
 			carry /= 10;
@@ -54,8 +53,6 @@ void multiply(char* s1, char* s2)
 		if (carry > 0)
 			result[i] += carry;
 	}
-
-	/* Print the result to stdout */
 	leading_zeros = true;
 	for (i = 0; i < (full_len - 1); i++)
 	{
@@ -68,17 +65,16 @@ void multiply(char* s1, char* s2)
 		printf("0");
 	printf("\n");
 
-	/* Free the memory used by the result array */
 	free(result);
 }
 
-/*
+/**
  * main - Entry point
  * @argc: The number of input arguments
  * @argv: An array containing the input arguments
  * Return: 0 if the multiplication was successful, 1 otherwise
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	/* Check if the input arguments are valid */
 	if (argc != 3 || !is_valid_input(argv[1]) || !is_valid_input(argv[2]))
@@ -86,9 +82,8 @@ int main(int argc, char* argv[])
 		printf("Error\n");
 		return (1);
 	}
-	
 	/* Perform the multiplication and print the result */
 	multiply(argv[1], argv[2]);
-	
+
 	return (0);
 }
