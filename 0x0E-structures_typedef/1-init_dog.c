4c8f0983f9a5d9db6a0ed5820365dd06c55397c5
Lionel <ndabagajeanlionel@gmail.com>
2023-04-18 00:27:13 +0000

    Fixed assignment issues

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - This function initializes the dog structure
+ * @d: Is the pointer to the new dog
+ * @name: Is the name to be given to the dog
+ * @age: Is the age of the new dog
+ * @owner: Is the owner of the new dog
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
