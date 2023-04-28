36c4184f5aaa9ee2a1d5de9d2bfb7fcb58ee44b2
Lionel <ndabagajeanlionel@gmail.com>
2023-04-28 03:11:58 +0000

    Pre printing completing

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * pre_main - This function prints the following sentence before the main() executes
+ * Return: Nothing
+ */
+void pre_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
