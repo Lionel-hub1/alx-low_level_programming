8ad0009ca6d1a640f35bbaf064be9d7694982bdf
Lionel <ndabagajeanlionel@gmail.com>
2023-04-28 02:57:45 +0000

    Freeing successfully

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - This function frees a linked list.
+ * @head: Is a pointer to the beginning of the list.
+ *
+ * Return: Nothing
+ */
+void free_list(list_t *head)
+{
+    list_t *temp;  /* Temporary variable to store the next node to free */
+
+    if (head == NULL)
+	    return;
+
+    while (head)
+    {
+        temp = head->next;  /* Store the next node before freeing node */
+        free(head->str);  /* Free the string inside the current node */
+        free(head);  /* Free the current node */
+        head = temp;  /* Move to the next node */
+    }
+}
+
