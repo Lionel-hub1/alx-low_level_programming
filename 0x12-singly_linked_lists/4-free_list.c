08a6e2c85ae1f9e122664589357d8d055c2724ff
Lionel <ndabagajeanlionel@gmail.com>
2023-04-28 02:59:57 +0000

    Betty issues fixed

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,17 +9,17 @@
  */
 void free_list(list_t *head)
 {
-    list_t *temp;  /* Temporary variable to store the next node to free */
+	list_t *temp;  /* Temporary variable to store the next node to free */
 
-    if (head == NULL)
-	    return;
+	if (head == NULL)
+		return;
 
-    while (head)
-    {
-        temp = head->next;  /* Store the next node before freeing node */
-        free(head->str);  /* Free the string inside the current node */
-        free(head);  /* Free the current node */
-        head = temp;  /* Move to the next node */
-    }
+	while (head)
+	{
+		temp = head->next;  /* Store the next node before freeing node */
+		free(head->str);  /* Free the string inside the current node */
+		free(head);  /* Free the current node */
+		head = temp;  /* Move to the next node */
+	}
 }
 
